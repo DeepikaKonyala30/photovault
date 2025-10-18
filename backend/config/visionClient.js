dc05c6e1ece077325d10828aaf1233a349dd19e8
Deepika Konyala <deepikakonyala30@gmail.com>
2025-10-18 19:57:21 +0530

    visionAPI changed

--- backend/config/visionClient.js
+++ backend/config/visionClient.js
@@ -1,7 +1,12 @@
 import { ImageAnnotatorClient } from "@google-cloud/vision";
-import path from "path";
 
-const keyPath = path.join(process.cwd(), "config", "gcloud-key.json");
-const client = new ImageAnnotatorClient({ keyFilename: keyPath });
+// Use credentials from environment variable instead of local file
+if (!process.env.GCLOUD_JSON) {
+  throw new Error("GCLOUD_JSON environment variable is missing!");
+}
+
+const credentials = JSON.parse(process.env.GCLOUD_JSON);
+
+const client = new ImageAnnotatorClient({ credentials });
 
 export default client;
